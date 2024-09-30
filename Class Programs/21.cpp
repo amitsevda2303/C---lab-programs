541a36c0880df90e20576460d5ae3f1e45524d35
amitsevda2303 <rohitkmt882gmail.com>
2024-09-30 15:53:25 +0530

    overloading program

---
+++ Class Programs/21.cpp
@@ -0,0 +1,37 @@
+// operator overloading using unary operator with member function
+
+#include <iostream>
+using namespace std;
+
+
+class Test {
+    private:
+    int a,b,c;
+    public:
+        void getdata(int x,int y,int z){
+            a = x;
+            b = y;
+            c = z;
+        }
+        void  operator -(){
+            a = -a;
+            b = -b;
+            c = -c;
+        }
+        void display(){
+            cout<<"A = "<<a<<endl;
+            cout<<"B = "<<b<<endl;
+            cout<<"C = "<<c<<endl;
+        }
+};
+
+
+int main(){
+    Test t1;
+    t1.getdata(10,20,30);
+    cout<<"Before Operator overloading "<<endl;
+    t1.display();
+    -t1;
+    cout<<"After Operator overloading "<<endl;
+    t1.display();
+}
