3d59b10e84d0a2e37f3646215bca24e15c14ca1b
amitsevda2303 <rohitkmt88@gmail.com>
2024-10-14 15:36:37 +0530

    do some changes

--- Class Programs/21.cpp
+++ Class Programs/21.cpp
@@ -3,35 +3,42 @@
 #include <iostream>
 using namespace std;
 
+class Test
+{
+private:
+    int a, b, c;
 
-class Test {
-    private:
-    int a,b,c;
-    public:
-        void getdata(int x,int y,int z){
-            a = x;
-            b = y;
-            c = z;
-        }
-        void  operator -(){
-            a = -a;
-            b = -b;
-            c = -c;
-        }
-        void display(){
-            cout<<"A = "<<a<<endl;
-            cout<<"B = "<<b<<endl;
-            cout<<"C = "<<c<<endl;
-        }
+public:
+    void getdata(int x, int y, int z)
+    {
+        a = x;
+        b = y;
+        c = z;
+    }
+    void operator-();
+    void display()
+    {
+        cout << "A = " << a << endl;
+        cout << "B = " << b << endl;
+        cout << "C = " << c << endl;
+    }
 };
 
+void Test ::operator-()
+{
 
-int main(){
+    a = -a;
+    b = -b;
+    c = -c;
+}
+
+int main()
+{
     Test t1;
-    t1.getdata(10,20,30);
-    cout<<"Before Operator overloading "<<endl;
+    t1.getdata(10, 20, 30);
+    cout << "Before Operator overloading " << endl;
     t1.display();
     -t1;
-    cout<<"After Operator overloading "<<endl;
+    cout << "After Operator overloading " << endl;
     t1.display();
 }
